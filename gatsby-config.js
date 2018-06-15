27c4dea1d9aeaba31fda4265111d2c72dd45320f
Evgeni Nikolov <evgeni.tsn@gmail.com>
2018-06-16 01:45:39 +0300

    Added Firebase integration

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,20 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-firebase`,
+      options: {
+        credential: require('./firebase-key.json'),
+        databaseURL: 'https://mashmo-on-gatsby-dev.firebaseio.com/',
+        types: [
+          {
+            type: 'Products',
+            path: 'products',
+          },
+        ],
+      },
+    },
+  ],
 }
