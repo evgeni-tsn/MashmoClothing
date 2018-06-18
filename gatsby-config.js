1b2ecdaf2cdb5903e7b1f27ee05076dd71500d46
Evgeni Nikolov <evgeni.nikolov@eNikolov.local>
2018-06-18 16:56:37 +0300

    Added offline support and manifest file

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,9 +6,28 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'GatsbyJS',
+        short_name: 'GatsbyJS',
+        start_url: '/',
+        background_color: '#f7f0eb',
+        theme_color: '#a2466c',
+        display: 'minimal-ui',
+        icon: 'src/images/icon.png', // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-offline',
+    {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography.js`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        color: `tomato`,
       },
     },
     {
