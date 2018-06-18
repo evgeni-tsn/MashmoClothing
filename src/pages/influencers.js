4b3ea2b16321bcadbbfc3caf4208e9f532c1174b
Evgeni Nikolov <evgeni.nikolov@eNikolov.local>
2018-06-18 15:27:38 +0300

    Added empty pages

---
+++ src/pages/influencers.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Influencers = () => (
+  <div>
+    <h1>Influencers</h1>
+  </div>
+)
+
+export default Influencers
---
+++ src/pages/shipping.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Shipping = () => (
+  <div>
+    <h1>Shipping</h1>
+  </div>
+)
+
+export default Shipping
