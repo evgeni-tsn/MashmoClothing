cf81c6758784af1b842d1e2bb0641dd6c3bf548e
Evgeni Nikolov <evgeni.tsn@gmail.com>
2018-06-19 02:24:28 +0300

    Removed localstorage for now

--- src/templates/product.js
+++ src/templates/product.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import Helmet from 'react-helmet'
-const items = JSON.parse(localStorage.getItem('cart')) || []
 
 export default ({ data }) => {
   const productData = data.products
@@ -9,18 +7,7 @@ export default ({ data }) => {
       <h1>{productData.name}</h1>
       <p>Price: {productData.price}</p>
       <p>URL: {productData.slug}</p>
-      <button
-        onClick={() => {
-          //Need to add / update quantity
-          //Remove items
-          const item = { slug: productData.slug }
-          items.push(item)
-          localStorage.setItem('items', JSON.stringify(items))
-          console.log(localStorage.getItem('cart'))
-        }}
-      >
-        Add to Cart
-      </button>
+      <button>Add to Cart</button>
     </div>
   )
 }
