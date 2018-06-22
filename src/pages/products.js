5489ae00c96669c4a739c0ec1b18901206a03cbf
Evgeni Nikolov <evgeni.tsn@gmail.com>
2018-06-22 16:58:44 +0300

    Added Product Card Component

--- src/pages/products.js
+++ src/pages/products.js
@@ -3,18 +3,66 @@ import Link from 'gatsby-link'
 import 'react-simple-flex-grid/lib/main.css'
 import { Row, Col } from 'react-simple-flex-grid'
 import H1 from '../components/styled/H1'
+import { StyledLink } from '../components/styled/StyledLink'
+
+import styled from 'styled-components'
+import colors from '../utils/colors'
+import productPlaceholder from '../images/productPlaceholder.png'
+
+const ProductCardContainer = styled.div`
+  border: 2px solid ${colors.grey};
+`
+const ProductCardImage = styled.img``
+
+const ProductCardInfo = styled.div`
+  margin-top: 0.4rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 0.6rem;
+`
+
+const ProductTitle = styled.h3`
+  margin-bottom: 0.8rem;
+  font-size: 1.8rem;
+  font-weight: lighter;
+`
+
+const ProductPrice = styled.p`
+  color: ${colors.main};
+  font-size: 1.3rem;
+`
+
+const ProductCardFooter = styled.div`
+  padding-top: 0.5rem;
+  padding-bottom: 0.5rem;
+  text-align: center;
+  font-weight: lighter;
+  font-size: 0.9rem;
+  background-color: ${colors.grey};
+`
 
 const Products = ({ data }) => (
   <div>
     <H1 underlined>Products</H1>
     <Row gutter={40}>
       {data.allProducts.edges.map(({ node }) => (
-        <Col span={6} key={node.id}>
-          <div style={{ border: '1px solid black', marginBottom: 20 }}>
-            <h3>{node.name}</h3>
-            <p>Price: {node.price}</p>
-            <Link to={node.slug}>URL: {node.slug}</Link>
-          </div>
+        <Col span={4} key={node.id}>
+          <ProductCardContainer>
+            <ProductCardImage src={'https://picsum.photos/500?random'} />
+            <StyledLink to={node.slug}>
+              <ProductCardInfo>
+                <ProductTitle>{node.name}</ProductTitle>
+                <ProductPrice>${node.price}</ProductPrice>
+              </ProductCardInfo>
+            </StyledLink>
+            <StyledLink to={node.slug}>
+              <ProductCardFooter>
+                <p>Details</p>
+              </ProductCardFooter>
+            </StyledLink>
+          </ProductCardContainer>
         </Col>
       ))}
     </Row>
