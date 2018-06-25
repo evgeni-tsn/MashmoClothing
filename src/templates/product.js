e88911e687a62f540d078640fbe97ef3d7858b51
Evgeni Nikolov <evgeni.tsn@gmail.com>
2018-06-26 01:38:32 +0300

    Fixed bugs, Started Product Page layout

--- src/templates/product.js
+++ src/templates/product.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import H1 from '../components/styled/H1'
 import FeaturedSection from '../components/FeaturedSection'
+import styled from 'styled-components'
+
+import 'react-simple-flex-grid/lib/main.css'
+import { Row, Col } from 'react-simple-flex-grid'
+
+const Image = styled.img`
+  max-width: 300px;
+`
+const SmallImage1 = styled.img`
+  max-width: 145px;
+`
+const SmallImage2 = styled(SmallImage1)`
+  margin-left: 10px;
+`
 
 class ProductTemplate extends React.Component {
   state = {
     quantityValue: 1,
-    disabledButton: false,
+    minimumQuantity: 1,
+    maximumQuantity: 8,
+    disableMinusButton: true,
+    disablePlusButton: false,
     errorMsgShow: false,
   }
 
@@ -13,6 +30,7 @@ class ProductTemplate extends React.Component {
     if (typeof window !== 'undefined' && window.localStorage) {
       let oldItems = JSON.parse(localStorage.getItem('cart')) || []
       let updateQuantity = false
+      //TODO: Add only needed data not all (remove images for ex.)
       for (const savedItem of oldItems) {
         if (savedItem.contentful_id === productData.contentful_id) {
           updateQuantity = true
@@ -27,12 +45,37 @@ class ProductTemplate extends React.Component {
     }
   }
 
-  handleChange(event) {
-    this.setState({ quantityValue: event.target.value }, () => {
-      Number(this.state.quantityValue) < 1
-        ? this.setState({ disabledButton: true, errorMsgShow: true })
-        : this.setState({ disabledButton: false, errorMsgShow: false })
-    })
+  checkOutOfQuantityRange() {
+    Number(this.state.quantityValue) <= this.state.minimumQuantity
+      ? this.setState({ disableMinusButton: true })
+      : this.setState({ disableMinusButton: false })
+    Number(this.state.quantityValue) < this.state.minimumQuantity
+      ? this.setState({ errorMsgShow: true })
+      : this.setState({ errorMsgShow: false })
+    Number(this.state.quantityValue) >= this.state.maximumQuantity
+      ? this.setState({ disablePlusButton: true })
+      : this.setState({ disablePlusButton: false })
+    Number(this.state.quantityValue) > this.state.maximumQuantity
+      ? this.setState({ errorMsgShow: true })
+      : this.setState({ errorMsgShow: false })
+  }
+
+  decreaseQuantity() {
+    this.setState(
+      currState => {
+        quantityValue: currState.quantityValue -= 1
+      },
+      () => this.checkOutOfQuantityRange()
+    )
+  }
+
+  increaseQuantity() {
+    this.setState(
+      currState => {
+        quantityValue: currState.quantityValue += 1
+      },
+      () => this.checkOutOfQuantityRange()
+    )
   }
 
   render() {
@@ -40,23 +83,62 @@ class ProductTemplate extends React.Component {
     const allProducts = this.props.data.allContentfulProduct
     return (
       <div>
-        <H1 underlined>{productData.name}</H1>
-        <p>Price: {productData.price}</p>
-        <p>URL: {productData.slug}</p>
-        <input
-          type="number"
-          min={1}
-          value={this.state.quantityValue}
-          onChange={e => this.handleChange(e)}
-        />
+        <Row justify={'center'}>
+          <Col xs={12} sm={12} md={6} lg={6} xl={6}>
+            <Row justify={'center'}>
+              <Col offset={2} span={8}>
+                <Image
+                  resolutions={productData.mainImages[0].sizes}
+                  src={productData.mainImages[0].resolutions.src}
+                />
+              </Col>
+            </Row>
+            <Row justify={'center'}>
+              <Col offset={2} span={8}>
+                <SmallImage1
+                  resolutions={productData.mainImages[0].sizes}
+                  src={productData.mainImages[0].resolutions.src}
+                />
+                <SmallImage2
+                  resolutions={productData.mainImages[1].sizes}
+                  src={productData.mainImages[1].resolutions.src}
+                />
+              </Col>
+            </Row>
+          </Col>
+          <Col span={6}>
+            <H1 underlined>{productData.name}</H1>
+            <h3>Price: {productData.price}</h3>
+            <span>Quantity: </span>
+            <button
+              onClick={() => this.decreaseQuantity()}
+              disabled={this.state.disableMinusButton}
+            >
+              -
+            </button>
+            <span>{this.state.quantityValue}</span>
+            <button
+              onClick={() => this.increaseQuantity()}
+              disabled={this.state.disablePlusButton}
+            >
+              +
+            </button>
+            <br />
+            <button
+              disabled={
+                this.state.quantityValue < this.state.minimumQuantity ||
+                this.state.quantityValue > this.state.maximumQuantity
+              }
+              onClick={() => this.addToCart(productData)}
+            >
+              Add to Cart
+            </button>
+            {this.state.errorMsgShow && (
+              <p>Quantity must be between 1 and 8 items.</p>
+            )}
+          </Col>
+        </Row>
         <br />
-        <button
-          disabled={this.state.disabledButton}
-          onClick={() => this.addToCart(productData)}
-        >
-          Add to Cart
-        </button>
-        {this.state.errorMsgShow && <p>Please add at least one item</p>}
         <FeaturedSection filterOut={productData} allProducts={allProducts} />
       </div>
     )
