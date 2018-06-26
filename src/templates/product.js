fd77aa1c22486e605f3ff008e592f7aab71b5afd
Evgeni Nikolov <evgeni.tsn@gmail.com>
2018-06-26 21:58:52 +0300

    Added height

--- src/templates/product.js
+++ src/templates/product.js
@@ -170,7 +170,7 @@ export const productQuery = graphql`
           createdAt
           mainImage {
             id
-            resolutions(width: 800, quality: 80) {
+            resolutions(width: 800, height: 800, quality: 80) {
               src
               tracedSVG
             }
