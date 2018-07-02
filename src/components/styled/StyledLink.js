47fee0ec501485d14ef06884d5c5536d0bb9ab44
Evgeni Nikolov <evgeni.tsn@gmail.com>
2018-07-02 16:13:49 +0300

    Smaller Font-size of header links

--- src/components/styled/StyledLink.js
+++ src/components/styled/StyledLink.js
@@ -4,7 +4,7 @@ import colors from '../../utils/colors'
 
 export const StyledLink = styled(Link)`
   text-decoration: none;
-  font-size: 1.1rem;
+  font-size: 1rem;
   color: ${({ light }) => (light ? colors.white : colors.black)};
   font-weight: bold;
 `
