4912bef5a0d3249bdfc3dd109eb84331cf307864
Evgeni Nikolov <evgeni.tsn@gmail.com>
2018-07-04 19:44:35 +0300

    Read hero image from contentful

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import styled from 'styled-components'
+import Img from 'gatsby-image'
 
 import Header from '../components/Header'
 import Footer from '../components/Footer'
@@ -13,33 +14,53 @@ import heroImage from '../images/hero.png'
 const MainContainer = styled(Container)`
   min-height: 75vh;
 `
-const Layout = ({ children, data, location }) => (
-  <div>
-    <Helmet
-      title={data.site.siteMetadata.title}
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    >
-      <link
-        href="https://fonts.googleapis.com/css?family=Montserrat"
-        rel="stylesheet"
-      />
-      <html lang="en" />
-    </Helmet>
-    <Header />
-    <main>
-      {location.pathname === '/' && (
-        <div>
-          <img src={heroImage} alt={'Hero Image'} />
-        </div>
-      )}
-      <MainContainer>{children()}</MainContainer>
-    </main>
-    <Footer />
-  </div>
-)
+
+//TODO: Experimental
+const GatsbyImage = styled(Img)`
+  width: 100%;
+  height: 440px;
+
+  @media only screen and (max-width: 1220px) {
+    height: 200px;
+  }
+  @media only screen and (max-width: 580px) {
+    height: 100px;
+  }
+`
+const Layout = ({ children, data, location }) => {
+  const heroImageData = data.allContentfulHeroImage.edges[0].node.heroImage
+  const siteTitleData = data.site.siteMetadata.title
+  console.log(heroImageData)
+  return (
+    <div>
+      <Helmet
+        title={siteTitleData}
+        meta={[
+          { name: 'description', content: 'Sample' },
+          { name: 'keywords', content: 'sample, something' },
+        ]}
+      >
+        <link
+          href="https://fonts.googleapis.com/css?family=Montserrat"
+          rel="stylesheet"
+        />
+        <html lang="en" />
+      </Helmet>
+      <Header />
+      <main>
+        {console.log()}
+        {location.pathname === '/' && (
+          <GatsbyImage
+            resolutions={heroImageData.resolutions}
+            alt={'Hero Image'}
+          />
+        )}
+        <MainContainer>{children()}</MainContainer>
+      </main>
+      <Footer />
+    </div>
+  )
+}
 
 Layout.propTypes = {
   children: PropTypes.func,
@@ -52,6 +73,19 @@ export const query = graphql`
     site {
       siteMetadata {
         title
+      }
+    }
+    allContentfulHeroImage {
+      edges {
+        node {
+          heroImage {
+            title
+            resolutions(width: 1920, quality: 70) {
+              tracedSVG
+              src
+            }
+          }
+        }
       }
     }
   }
