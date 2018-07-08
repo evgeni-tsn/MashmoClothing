8a43a211ed3b883a3c6d9fe9ecf3728ab5f21a01
Evgeni Nikolov <evgeni.tsn@gmail.com>
2018-07-08 22:39:34 +0300

    Changed Quantity Buttons

---
+++ src/components/styled/QuantityButton.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+import colors from '../../utils/colors'
+
+export default styled.button`
+  width: 2rem;
+  height: 2rem;
+  background-color: transparent;
+  border: 3px solid ${colors.main};
+`
