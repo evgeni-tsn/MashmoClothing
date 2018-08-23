93f9cce7f1e75c7108c65520bce3ddf86652d5ac
Evgeni Nikolov <evgeni.tsn@gmail.com>
2018-08-23 20:09:10 +0300

    Created 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import styled from 'styled-components'
 
+import { GhostButtonLink } from '../components/styled'
+
+import notFoundImage from '../images/404.png'
+
+const NotFoundContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
 const NotFoundPage = () => (
-  <div>
+  <NotFoundContainer>
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
+    <img src={notFoundImage} style={{ marginTop: '2.5rem' }} />
+    <GhostButtonLink
+      to="/products"
+      style={{ fontSize: '1.4rem', marginTop: '2.5rem' }}
+    >
+      Обратно към продуктите
+    </GhostButtonLink>
+  </NotFoundContainer>
 )
 
 export default NotFoundPage
