b2ab80be59719d6955a74dc60c9af83b8666b033
Evgeni Nikolov <evgeni.tsn@gmail.com>
2018-10-14 16:36:16 +0300

    Added Fav Icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,24 @@ module.exports = {
         icon: 'src/images/icon.png', // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-plugin-favicon',
+      options: {
+        logo: './src/images/favicon.png',
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: false,
+          yandex: false,
+          windows: false,
+        },
+      },
+    },
     'gatsby-plugin-offline',
     'gatsby-plugin-netlify-cache',
     {
