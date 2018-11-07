3e19db546508b1de4c81bc946725c173734f7bbb
Evgeni Nikolov <evgeni.tsn@gmail.com>
2018-11-07 02:27:48 +0200

    Swap gatsby plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,22 +34,22 @@ module.exports = {
         accessToken: process.env.CONTENTFUL_TOKEN,
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: 'Mashmo Clothing',
-    //     short_name: 'Mashmo',
-    //     description: 'Mashmo Clothing Online Store',
-    //     start_url: '/',
-    //     background_color: '#BFA26E',
-    //     theme_color: '#2A2A2A',
-    //     display: 'standalone',
-    //     icon: 'src/images/favicon.png', // This path is relative to the root of the site.
-    //   },
-    // },
     `gatsby-plugin-sitemap`,
     'gatsby-plugin-robots-txt',
     'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'Mashmo Clothing',
+        short_name: 'Mashmo',
+        description: 'Mashmo Clothing Online Store',
+        start_url: '/',
+        background_color: '#BFA26E',
+        theme_color: '#2A2A2A',
+        display: 'standalone',
+        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
+      },
+    },
     'gatsby-plugin-netlify-cache',
     {
       resolve: `gatsby-plugin-typography`,
