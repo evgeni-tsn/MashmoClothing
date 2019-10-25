2b93514dc7abfbeb43f5b8fbf87b7d4f51c604a2
Evgeni Nikolov <evgeni.tsn@gmail.com>
2019-10-26 01:55:05 +0300

    remove uninstalled plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,6 @@ module.exports = {
         icon: 'src/images/favicon.png', // This path is relative to the root of the site.
       },
     },
-    'gatsby-plugin-netlify-cache',
     {
       resolve: `gatsby-plugin-typography`,
       options: {
